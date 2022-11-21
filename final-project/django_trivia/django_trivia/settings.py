d4df29ace52a7c15a9e8cd32a5b92b996805ad89
jtsmith11 <jtsmith11@my.waketech.edu>
2022-11-20 20:41:00 -0500

    Changed default port to 8080

--- final-project/django_trivia/django_trivia/settings.py
+++ final-project/django_trivia/django_trivia/settings.py
@@ -124,3 +124,5 @@ STATICFILES_DIRS=[BASE_DIR/'static']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+RUN_SERVER_PORT = 8080
