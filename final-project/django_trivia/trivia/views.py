6f0b025edb6da4739cff5cd03090bf4f61e3e806
jtsmith11 <jtsmith11@my.waketech.edu>
2022-11-27 04:41:12 -0500

    Changed home page, added questions missed to results page

--- final-project/django_trivia/trivia/views.py
+++ final-project/django_trivia/trivia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.http import HttpResponse
 import random
+import json
 
 # Create your views here.
 def home(request):
@@ -16,14 +17,33 @@ def quiz(request, genre = 'history', question_total = '20'):
         percentage = 0
         total = 0
         correct = 0
+        results_list = []
+
+        print(request.POST)
 
         for t in trivia_questions:
 
-            if t.correct ==  response_data.get(str(t.id)):
+            correct_status = False
+
+            if t.correct == response_data.get(str(t.id)):
                 correct+=1
+                correct_status = True
 
             if str(t.id) in response_data:
                 total+=1
+
+                if correct_status == True:
+                    status = "Correct"
+                else:
+                    status = "False"
+
+                results_list.append({"question":t.question,
+                                   "yourAnswer":response_data.get(str(t.id)),
+                                   "correctAnswer":t.correct,
+                                    "status":status})
+
+        print("2")
+
 
         percentage = round(((correct / total) * 100), 2)
 
@@ -31,6 +51,7 @@ def quiz(request, genre = 'history', question_total = '20'):
             'percentage':percentage,
             'total':total,
             'correct':correct,
+            'results':results_list
         }
 
         return render(request, 'trivia/results.html', context)
